9c6d244cfb24c09e519557033eb0eccc878cbd57
rodionkuhty <rodionkuhty@gmail.com>
2026-04-14 18:42:50 +0300

    remove unused files

--- src/app/preload.js
+++
@@ -1,9 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-contextBridge.exposeInMainWorld('electronAPI', {
-    sendMessage: (message) => ipcRenderer.invoke('agent-chat', message),
-    onUpdate: (callback) => {
-        const listener = (_event, update) => callback(update);
-        ipcRenderer.on('agent-update', listener);
-        return () => ipcRenderer.removeListener('agent-update', listener);
-    },
-});
